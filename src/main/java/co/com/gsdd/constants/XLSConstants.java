ef776c06674809880facfb032ca8425a1ad51a4a
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-04-28 20:11:48 -0500

    Init repo

---
+++ src/main/java/co/com/gsdd/constants/XLSConstants.java
@@ -0,0 +1,12 @@
+package co.com.gsdd.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class XLSConstants {
+
+    public static final String EXT_XLS = ".xls";
+    public static final String EXT_XLSX = ".xlsx";
+
+}
